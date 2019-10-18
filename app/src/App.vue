27d33830cdda3a62fb3f6cb53389390b0431185b
bashuang <53343306+bashuang@users.noreply.github.com>
2019-10-18 20:24:58 +0800

    aa1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    这是aa分支
+    <Login></Login>
   </div>
 </template>
-
+<script>
+  import Login from './components/login.vue'
+    export default {
+        name: 'App',
+        components:{
+            Login
+        }
+    }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
